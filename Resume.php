0f0c906fdc097e5c5975bbe9ba07921129d655fd
ShyamBabu <39897932+SkillsHat@users.noreply.github.com>
2018-11-28 10:48:03 +0530

    Create Resume Class file

---
+++ Resume.php
@@ -0,0 +1,32 @@
+<?php
+
+class Resume
+{
+    function readDocx($filename){
+
+        $striped_content = '';
+        $content = '';
+
+        if(!$filename || !file_exists($filename)) return false;
+
+        $zip = zip_open($filename);
+        if (!$zip || is_numeric($zip)) return false;
+
+        while ($zip_entry = zip_read($zip)) {
+
+            if (zip_entry_open($zip, $zip_entry) == FALSE) continue;
+
+            if (zip_entry_name($zip_entry) != "word/document.xml") continue;
+
+            $content .= zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+
+            zip_entry_close($zip_entry);
+        }
+        zip_close($zip);
+        $content = str_replace('</w:r></w:p></w:tc><w:tc>', " ", $content);
+        $content = str_replace('</w:r></w:p>', "\r\n", $content);
+        $striped_content = strip_tags($content);
+
+        return $striped_content;
+    }
+}
